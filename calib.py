7aaf706df3a3f4d9917b6419ff37b55e1b5af20e
Friederike Weilbeer <friederike.weilbeer@gmail.com>
2023-03-27 12:57:43 +0200

    add new modules

---
+++ calib.py
@@ -0,0 +1,66 @@
+import numpy as np
+import cv2
+import os
+import argparse
+
+
+def calibrate(dirpath, square_size, width, height, visualize=False):
+    """ Apply camera calibration operation for images in the given directory path. """
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(8,6,0)
+    objp = np.zeros((height * width, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2)
+
+    objp = objp * square_size
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    images = os.listdir(dirpath)
+
+    for fname in images:
+        img = cv2.imread(os.path.join(dirpath, fname))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (width, height), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret:
+            objpoints.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (width, height), corners2, ret)
+
+        if visualize:
+            cv2.imshow('img', img)
+            cv2.waitKey(0)
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    return [ret, mtx, dist, rvecs, tvecs]
+
+
+if __name__ == '__main__':
+    dirpath = 'calib_images/*.jpg'
+    # 2.2 cm == 0.022 m
+    # square_size = 0.022
+    square_size = 0.022
+    width = 9
+    height = 6
+    visualize = False
+
+    ret, mtx, dist, rvecs, tvecs = calibrate(dirpath, square_size, visualize=visualize, width=width, height=height)
+
+    print(mtx)
+    print(dist)
+
+    np.save("calibration_matrix", mtx)
+    np.save("distortion_coefficients", dist)
